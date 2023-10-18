0396c25f58649da1885c36c3d35adbc9172a04ea
Ruslan Vovchok <Ruslan.Vovchok@flex.com>
2023-10-18 20:23:34 +0300

    First Lab

---
+++ Lab1/src/Application.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class Application {
+	
+	public static void main(String[] arqs) {
+		System.out.println("Hello World!");
+		
+		Scanner scanner = new Scanner(System.in);
+		String userName = scanner.nextLine();
+		
+		System.out.println("Hello " + userName + "! Nice to meet you");
+	}
+	
+}
